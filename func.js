41945a2990f17a3d238cfa3bfe44689ef143da22
James Akpan <arfitcode@gmail.com>
2019-06-08 22:19:29 +0100

    first database fix

--- func.js
+++ func.js
@@ -1,5 +1,3 @@
-// import  words from "./dict"
-
 let body  = $("body");
 
 
@@ -18,61 +16,109 @@ meaning.hide()
 let backToMeaning = $(meaning.children("h1"))
 
 
-
+  
 
 searchButton.on("click", ()=> {
 
-    let newWord = usersWord.val().toLowerCase();
+  
 
-    if( words[newWord].toLowerCase()){
+// A variable to save the lowercase of usersword
+let userWordtolowercase = usersWord.val().toLowerCase()
+
+// A reference of the usersword.
+let getUserWord = ref.collection("dictionary").where("names" , "==" ,  userWordtolowercase).get()
+
+// Get the snapshot of that document.
+getUserWord.then(snapshot => {
 
 
-        main.animate({height: "20vh"}, 'slow');
+  // If it;s empty then return something like so
+  if(snapshot.empty){
+    console.log("No data")
+    main.animate({height: "20vh"}, 'slow');
+    inputBox.animate({opacity: "0.00003"}, 400)
+    meaning.show(300)
 
-        // Hide the input area
-        inputBox.animate({opacity: "0.00003"}, 400)
+    meaning.children("h3").text("Word not Found");
+    meaning.children("#word").html(`Oh we couldn't find your word! ${`<a style="color: #211F97; font-weight: 900" href=" https://www.google.com/search?client=firefox-b-d&q=${usersWord.val()} "> Click Here</a>` } to find it on GOOGLE, Goolge has all the answers after all. `)
 
-        // Show the meaning section
-        meaning.show(300)
 
-        // Edit texts
-        meaning.children("h3").text(usersWord.val().toUpperCase())
-        meaning.children("#word").text(words[usersWord.val().toLowerCase()])
+  }
+
+  // But if not empty then loop through the doc and it 
+  // returns a data which you can access at every looop. 
+
+  else{
+
+    main.animate({height: "20vh"}, 'slow');
+    // Hide the input area
+    inputBox.animate({opacity: "0.00003"}, 400)
+
+    // Show the meaning section
+    meaning.show(300)
+
+  snapshot.forEach( 
+
+    doc =>{ 
+
+      // that's an object.
+      let returnedWord = doc.data()
+
+       // Edit texts
+      meaning.children("h3").text(usersWord.val().toUpperCase())
+      meaning.children("#word").text(Object.values(returnedWord)[0])
     }
+  )
 
-   
-
-   
-
-    else{
-
-        main.animate({height: "20vh"}, 'slow');
-        inputBox.animate({opacity: "0.00003"}, 400)
-        meaning.show(300)
-
-        meaning.children("h3").text("Word not Found");
-        meaning.children("#word").html(`Oh we couldn't find your word! ${`<a style="color: #211F97; font-weight: 900" href=" https://www.google.com/search?client=firefox-b-d&q=${usersWord.val()} "> Click Here</a>` } to find it on GOOGLE, Goolge has all the answers after all. `)
+  }
+})
 
 
-        
-       
 
-    }
+  // let newWord = usersWord.val().toLowerCase();
+
+  // if( words[newWord].toLowerCase()){
+
+
+  //     main.animate({height: "20vh"}, 'slow');
+
+  //     // Hide the input area
+  //     inputBox.animate({opacity: "0.00003"}, 400)
+
+  //     // Show the meaning section
+  //     meaning.show(300)
+
+  //     // Edit texts
+  //     meaning.children("h3").text(usersWord.val().toUpperCase())
+  //     meaning.children("#word").text(words[usersWord.val().toLowerCase()])
+  // }
+
+ 
+
+ 
+
+  // else{
 
     
-   
+      
+     
 
-   
+  // }
 
-    
+  
+ 
+
+ 
+
+  
 })
 
 backToMeaning.on("click", ()=> {
-    inputBox.animate({opacity: "1"})
+  inputBox.animate({opacity: "1"})
 
-    meaning.hide(300)
+  meaning.hide(300)
 
-    main.animate({height: "100vh"}, 'slow');
+  main.animate({height: "100vh"}, 'slow');
 
 
 })
@@ -84,25 +130,67 @@ backToMeaning.on("click", ()=> {
 
 body.on("keyup", (e)=>{
 
-    let newWord = usersWord.val().toLowerCase()
+  let newWord = usersWord.val().toLowerCase()
 
-    if(words[newWord]){
+  if(words[newWord]){
 
-    if(e.code === "Enter"){
-        main.animate({height: "20vh"}, 'slow');
-        inputBox.animate({opacity: "0.00003"}, 400)
+  if(e.code === "Enter"){
+      main.animate({height: "20vh"}, 'slow');
+      inputBox.animate({opacity: "0.00003"}, 400)
 
 
-    
-        meaning.show(300)
+  
+      meaning.show(300)
 
-        meaning.children("h3").text(usersWord.val().toUpperCase())
+      meaning.children("h3").text(usersWord.val().toUpperCase())
 
-        meaning.children("p").text(words[usersWord.val().toLowerCase()])
-    }
+      meaning.children("p").text(words[usersWord.val().toLowerCase()])
+  }
 
 
 
 }
 })
 
+
+
+
+
+
+
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAIyR3YFeLcMF_1j9y7XMPoJVl_c4xR3r4",
+  authDomain: "dictionary01-fe872.firebaseapp.com",
+  databaseURL: "https://dictionary01-fe872.firebaseio.com",
+  projectId: "dictionary01-fe872",
+  storageBucket: "dictionary01-fe872.appspot.com",
+  messagingSenderId: "974905770546",
+  appId: "1:974905770546:web:1a56beca1e08b653"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+let ref = firebase.firestore();
+
+
+// for(let wordBoy in words){
+//   ref.collection("dictionary").doc().set
+//   (
+//     {names: wordBoy,
+//     definition: words[wordBoy] 
+//   }
+    
+//   )
+// } 
+
+// ref.collection("dictionary").doc("00DToeTRVW5d57l5JeVJ").get()
+// .then( doc =>{
+//   if(!doc.exists){
+//     console.log("No documents Oh!")
+//   }
+
+//   else{
+//     console.log(doc.data())
+//   }
+// } )
